3afe863f383a84a4af9c70c7c9b8851602cb6c07
algamawiy <aliyuassalafiygama@gmail.com>
2024-01-20 02:43:40 -0800

    Modified main.py file

--- app/main.py
+++ app/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
 from api.endpoints import stores
+from models import store
+from db.database import engine
 
 
 
 app = FastAPI()
+
+store.Base.metadata.create_all(bind=engine)
 
 app.include_router(stores.router)
